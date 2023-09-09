403a755a861e23970a0b6e76ac9153dd21a367f9
pdvbv macbook <piet.devisser@cheerful.com>
2023-09-09 17:47:55 +0200

    ore

---
+++ do_all.sh
@@ -0,0 +1,94 @@
+#!/bin/ksh
+
+# do_all.sh: loop over all nodes with a command...
+#
+# todo: HARDcoded nodenames TWICE: make a list.
+#
+# typical usage: measure disk-usage, or check tablets..
+# ./do_all.sh du -sh --inodes /root/var/data/yb-data/
+# ./do_all.sh yb-admin  -master_addresses node1:7100 list_tablets ysql.yugabyte t_1st 0 
+#
+
+#  verify first, show command
+
+echo .
+echo do_all: \[  $* \] ...  cockroach docker cluster 
+echo .
+
+# do it once, quick...
+for node in roach1 roach2 roach3 roach4 roach5 roach6 roach7
+do
+
+  echo doing node $node  
+  docker exec -it $node $*
+
+done
+
+
+echo .
+echo do_all.sh: 10 sec to Cntr-C .. or .. continue doing it slower forever...
+echo . 
+
+sleep 10
+
+# now loop slowly over nodes
+while true 
+do
+
+  echo .
+  echo do_all: \[  $* \] ... cockroach on docker
+  echo .
+
+  for node in roach1 roach2 roach3 roach4 roach5 roach6 roach7
+  do
+
+    echo .
+    echo doing node $node :
+    docker exec -it $node $*
+
+    sleep 2 
+
+  done
+
+  echo ----- do_all_clu.sh: cockroach loop over nodes done, next.. 
+  sleep 10
+
+done 
+
+# ----------------- end do_all_clu.sh -------------
+
+echo .
+echo notes: code should never get this far.. but keep as notes
+echo .
+
+sleep 10
+
+while true 
+do
+
+  echo node1:
+  docker exec node1  ps -ef  | grep database_host | cut -d= -f2  
+  docker exec -it node1 yugabyted status | grep atus; sleep 3 ; echo .
+
+  echo node2:
+  docker exec node2  ps -ef  | grep database_host | cut -d= -f2  
+  docker exec -it node2 yugabyted status | grep atus; sleep 3 ; echo .
+
+  echo node3:
+  docker exec node3  ps -ef  | grep database_host | cut -d= -f2  
+  docker exec -it node3 yugabyted status | grep atus; sleep 3 ; echo .
+
+  echo node4:
+  docker exec node4  ps -ef  | grep database_host | cut -d= -f2  
+  docker exec -it node4 yugabyted status | grep atus; sleep 6 ; echo .
+
+  echo node5:
+  docker exec node5  ps -ef  | grep database_host | cut -d= -f2  
+  docker exec -it node4 yugabyted status | grep atus; sleep 6 ; echo .
+
+  echo node6:
+  docker exec node6  ps -ef  | grep database_host | cut -d= -f2  
+  docker exec -it node4 yugabyted status | grep atus; sleep 6 ; echo .
+
+done 
+
